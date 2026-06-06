bb39c8ed45bfd71ff3b48b583be88b6109d010f9
marcilianocamargo-stack <marciliano.camargo@gmail.com>
2026-06-06 17:04:39 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'prime-etiquetas-v1';
+const ASSETS = [
+  '/etiquetas-Prime/',
+  '/etiquetas-Prime/index.html',
+  '/etiquetas-Prime/manifest.json',
+  '/etiquetas-Prime/icon-192.png',
+  '/etiquetas-Prime/icon-512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    )).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
